2d4b858b924092281995b7b244619ebbf31d1275
qhshef <qhshef@gmail.com>
2022-03-21 20:08:06 +0900

    Initial commit

---
+++ src/test/java/com/gguri/gr/GrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gguri.gr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
